72f05c345bbd6f2e03b32a1ac8449cfc5020226f
palbrcht <palbrcht@gmail.com>
2022-01-18 00:38:44 -0600

    added separations and headcount

--- mpd_churn.py
+++ mpd_churn.py
@@ -2,8 +2,17 @@
 import requests
 
 ### Query MPD Police Academy Applicants & Recruits API
-
 mpd_academy_api_string = 'https://data.memphistn.gov/resource/szjn-whbu.json'
 api_request = requests.get(mpd_academy_api_string)
-json_data = api_request.json()
-print(json_data)
+mpd_academy_data = api_request.json()
+
+###  Query MPD Commissioned Separations API
+mpd_academy_api_string = 'https://data.memphistn.gov/resource/63bs-dvpq.json'
+api_request = requests.get(mpd_academy_api_string)
+mpd_separations_data = api_request.json()
+
+###  Query MPD Headcount API
+mpd_academy_api_string = 'https://data.memphistn.gov/resource/iwk8-fxnz.json'
+api_request = requests.get(mpd_academy_api_string)
+mpd_headcount_data = api_request.json()
+print(mpd_headcount_data)
