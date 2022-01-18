f9fb2b0e0edbc3d1bbf87ebe7a7e7dc5573cd2df
palbrcht <palbrcht@gmail.com>
2022-01-18 00:32:52 -0600

    initial query

--- mpd_churn.py
+++ mpd_churn.py
@@ -1,1 +1,9 @@
 ###  Import Modules
+import requests
+
+### Query MPD Police Academy Applicants & Recruits API
+
+mpd_academy_api_string = 'https://data.memphistn.gov/resource/szjn-whbu.json'
+api_request = requests.get(mpd_academy_api_string)
+json_data = api_request.json()
+print(json_data)
